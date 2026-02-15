5bbb393cb6335d6416bd43f0159624ee21ccc313
Raphael_Mazini <raphaelmazini37@gmail.com>
2026-02-15 18:49:47 -0300

    update navbar end contact page jquery form -- 18:49 -- 15/02/2026

--- e_commerce/e_commerce/forms.py
+++ e_commerce/e_commerce/forms.py
@@ -30,6 +30,9 @@ class ContactForm(forms.Form):
     
     def clean_email(self):
         email = self.cleaned_data.get("email")
-        if not "gmail.com" in email:
-            raise forms.ValidationError("O Email deve ser do gmail.com")
+        if email and ("@" not in email or ".com" not in email):
+            raise forms.ValidationError("O Email deve ser válido !")
         return email
+
+    def clean_content(self):
+        raise forms.ValidationError("O conteúdo está errado.")
