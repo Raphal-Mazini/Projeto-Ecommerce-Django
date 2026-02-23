7e8454736f64d9dcafb3d808f84d2a168cac8e57
Raphael_Mazini <raphaelmazini37@gmail.com>
2026-02-23 15:04:18 -0300

    15:04 -- 23/02/2026

--- e_commerce/static_local/js/csrf.ajax.js
+++ e_commerce/static_local/js/csrf.ajax.js
@@ -1,41 +1,3 @@
-// This is the old way to do form ajax csrf token in django
-// $(document).ready(function(){
-//     // using jQuery
-//     function getCookie(name) {
-//       let cookieValue = null;
-//       if (document.cookie && document.cookie !== '') {
-//         const cookies = document.cookie.split(';');
-//         for (let i = 0; i < cookies.length; i++) {
-//           const cookie = jQuery.trim(cookies[i]);
-//           // Does this cookie string begin with the name we want?
-//           if (cookie.substring(0, name.length + 1) === (name + '=')) {
-//             cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-//             break;
-//           }
-//         }
-//       }
-//       return cookieValue;
-//     }
-//     const csrftoken = getCookie('csrftoken');
-  
-//     function csrfSafeMethod(method) {
-//       // these HTTP methods do not require CSRF protection
-//       return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-//     }
-//     $.ajaxSetup({
-//       beforeSend: function(xhr, settings) {
-        
-//         if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-//            console.log("Settings Type: " + settings.type)
-//            console.log("CSRF TOKEN: " + csrftoken)
-//            console.log("XHR: " + xhr.global)
-//            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-//         }
-//       }
-//     });
-//   })
-
-
 // This is the new way to do form ajax csrf token in django - Using Fetch API
 $(document).ready(function(){
   function getCookie(name) {
@@ -70,6 +32,6 @@ $(document).ready(function(){
       body: JSON.stringify(data)
     });
   }
-  //const url = window.location.pathname;
-  //sendData(url)
+  const url = window.location.pathname;
+  sendData(url)
 });
