bfd1761a1bd57d4b2466ea5841779e12fc01c753
Raphael_Mazini <raphaelmazini37@gmail.com>
2026-02-26 11:22:37 -0300

    update: accounts/forms.py;accounts/views.py;e_commerce/urls.py -- 11:22 -- 26/02/2026

--- e_commerce/accounts/forms.py
+++ e_commerce/accounts/forms.py
@@ -1,10 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
-
 User = get_user_model()
-
-
 
 class UserAdminCreationForm(forms.ModelForm):
     """
@@ -38,7 +35,6 @@ class UserAdminCreationForm(forms.ModelForm):
         return user
 
 
-
 class UserAdminChangeForm(forms.ModelForm):
     """A form for updating users. Includes all the fields on
     the user, but replaces the password field with admin's
@@ -55,32 +51,42 @@ class UserAdminChangeForm(forms.ModelForm):
         # This is done here, rather than on the field, because the
         # field does not have access to the initial value
         return self.initial["password"]
-    
-
-
-
-
-class LoginForm(forms.Form):
-    email = forms.EmailField(label="E-mail")
-    password = forms.CharField(widget=forms.PasswordInput, label="Senha")
-
-class RegisterForm(forms.Form):
-    full_name = forms.CharField(label="Nome Completo")
-    email     = forms.EmailField(label="E-mail")
-    password  = forms.CharField(widget=forms.PasswordInput, label="Senha")
-    password2 = forms.CharField(label='Confirme a Senha', widget=forms.PasswordInput)
-
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("Este e-mail já está cadastrado.")
-        return email
-
-    def clean(self):
-        data = self.cleaned_data
-        if data.get('password') != data.get('password2'):
-            raise forms.ValidationError("As senhas devem ser iguais.")
-        return data
 
 class GuestForm(forms.Form):
-    email = forms.EmailField(label="E-mail para convidado")
+    email = forms.EmailField()
+    
+class LoginForm(forms.Form):
+    email = forms.EmailField(label='Email')
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class RegisterForm(forms.ModelForm):
+    """
+    A form for creating new users. Includes all the required
+    fields, plus a repeated password.
+    """
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['email']
+
+    def clean(self):
+        '''
+        Verify both passwords match.
+        '''
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_2 = cleaned_data.get("password_2")
+        if password is not None and password != password_2:
+            self.add_error("password_2", "Your passwords must match")
+        return cleaned_data
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(RegisterForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        # user.active = False # send confirmation email
+        if commit:
+            user.save()
+        return user
