c4464345ac2aebdd413028e85f85f5564660a1d7
Raphael_Mazini <raphaelmazini37@gmail.com>
2026-02-27 09:33:26 -0300

    Projeto E-Commerce -- create a new app analitycs -- 27/02/2026 -- 09:33

--- e_commerce/e_commerce/settings.py
+++ e_commerce/e_commerce/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #our apps
+    #our apps.
+    'analytics',
     'addresses',
     'billing',
     'accounts',
